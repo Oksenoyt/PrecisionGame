7652d42a1046279e5aa88c18cf23aff9c18fb507
Oksenoyt <a.oksenoyt@gmail.com>
2022-09-20 13:37:36 +0400

    Initial Commit

---
+++ PrecisionGame/PrecisionGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrecisionGameApp.swift
+//  PrecisionGame
+//
+//  Created by Elenka on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct PrecisionGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
